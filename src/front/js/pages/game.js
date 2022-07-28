a25b93116163872607ac6bfa79a52b9809e05526
jsowers13 <jsowers13@gmail.com>
2022-07-28 03:12:50 +0000

    added blocking for Herbie

--- src/front/js/pages/game.js
+++ src/front/js/pages/game.js
@@ -7,38 +7,69 @@ export const Game = () => {
   const { store, actions } = useContext(Context);
   const [herbieHP, setHerbieHP] = useState(100);
   const [monsterHP, setMonsterHP] = useState(100);
+  const [activityText, setActivityText] = useState("");
+  const [herbieDefending, setHerbieDefending] = useState(false);
+  const maxHerbieHP = 100;
   var herbieAttack = () => {
     var atkPwr = Math.round(Math.random() * 10 + 1);
     setMonsterHP(monsterHP - atkPwr);
-    console.log("Herbie Attacked for " + atkPwr + " HP!");
-  };
-  var monsterAttack = () => {
-    var atkPwr = Math.round(Math.random() * 10 + 1);
-
-    setHerbieHP(herbieHP - atkPwr);
-    console.log("Monster Attack for " + atkPwr);
+    setActivityText(`Herbie Attacked for ${atkPwr} HP!`);
   };
   var herbieHeal = () => {
-    var healPwr = Math.round(Math.random() * 5 + 3);
-    setHerbieHP(herbieHP + healPwr);
-    console.log("Herbie Healed");
+    var healPwr = Math.round(Math.random() * (5 - 3 + 1)) + 3;
+
+    if (herbieHP + healPwr >= maxHerbieHP) {
+      setHerbieHP(maxHerbieHP);
+      setActivityText(`Herbie Healed to Full Health!`);
+    } else {
+      setHerbieHP(herbieHP + healPwr);
+      setActivityText(`Herbie Healed for ${healPwr} HP!`);
+    }
+  };
+  var herbieDefend = () => {
+    setHerbieDefending(true);
+    setActivityText(`Herbie is bracing for the next attack!`);
+  };
+  var monsterAttack = () => {
+    var diceRoll = Math.floor(Math.random() * 100) + 1;
+
+    var atkPwr = Math.round(Math.random() * 10) + 1;
+    var blockedPwr = Math.floor(atkPwr / 2);
+
+    if (herbieDefending) {
+      setHerbieHP(herbieHP - blockedPwr);
+      setActivityText(
+        `Monster Attacked! Herbie Blocked! ${blockedPwr} damage to Herbie.`
+      );
+    } else if (herbieDefending == false) {
+      setHerbieHP(herbieHP - atkPwr);
+      setActivityText(`Monster Attacked for ${atkPwr} HP!`);
+    }
+
+    setHerbieDefending(false);
   };
 
   return (
-    <div className="container-fluid d-flex mx-auto">
-      <div className="hero-box text-center">
-        <img src={rigoImageUrl} className="rigo-pic" />
-        <h1>Health Points: {herbieHP}</h1>
-        <button onClick={herbieAttack}>attack</button>
-        <button onClick={herbieHeal}>Heal</button>
+    <div className="container bg-danger d-flex flex-column align-items-center">
+      <div className="w-100 bg-primary d-flex">
+        <div className="hero-box text-center">
+          <img src={rigoImageUrl} className="rigo-pic" />
+          <h1>Health Points: {herbieHP}</h1>
+          <button onClick={herbieAttack}>attack</button>
+          <button onClick={herbieHeal}>Heal</button>
+          <button onClick={herbieDefend}>Block</button>
+        </div>
+        <div className="monster-box text-center">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/08/25/09/26/monster-426996__340.jpg"
+            className="monster-pic"
+          />
+          <h1>Health Points: {monsterHP}</h1>
+          <button onClick={monsterAttack}>Attack</button>
+        </div>
       </div>
-      <div className="monster-box text-center">
-        <img
-          src="https://cdn.pixabay.com/photo/2014/08/25/09/26/monster-426996__340.jpg"
-          className="monster-pic"
-        />
-        <h1>Health Points: {monsterHP}</h1>
-        <button onClick={monsterAttack}>Attack</button>
+      <div className="row bg-success text-center">
+        <h1 className="col-12">{activityText}</h1>
       </div>
     </div>
   );
